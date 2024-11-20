e6d19aa39b9113bf07658895201cf072d646168f
divyaSoni123 <147050056+divyaSoni123@users.noreply.github.com>
2024-11-20 13:17:31 +0530

    Delete NameAndColor.py

--- NameAndColor.py
+++
@@ -1,7 +0,0 @@
-# Get user input
-name = input("What is your name? ")
-color = input("What is your favrotie color? ")
-
-# Display greeting
-print(f"Hello, {name}!")
-print(f"You favorite color is {color}.")
