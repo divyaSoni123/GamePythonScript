23398765cc0821edc25dd33c90a64c8c94628f93
divyaSoni123 <147050056+divyaSoni123@users.noreply.github.com>
2024-11-20 13:11:00 +0530

    Create NameAndColor.py

---
+++ NameAndColor.py
@@ -0,0 +1,7 @@
+# Get user input
+name = input("What is your name? ")
+color = input("What is your favrotie color? ")
+
+# Display greeting
+print(f"Hello, {name}!")
+print(f"You favorite color is {color}.")
